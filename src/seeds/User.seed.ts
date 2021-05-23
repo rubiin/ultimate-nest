63fda17e0d60d4e0301bc9ac287e7455f0110f8a
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-23 16:37:45 +0545

    feat: added seeds

---
+++ src/seeds/User.seed.ts
@@ -0,0 +1,10 @@
+// import { EntityManager } from '@mikro-orm/core';
+// import { Seeder } from '@mikro-orm/seeder';
+// import { UserFactory } from 'factory/User.factory';
+
+// export class UserSeeder extends Seeder {
+
+//   async run(em: EntityManager): Promise<void> {
+//     new UserFactory(em).make(10)
+//   }
+// }
