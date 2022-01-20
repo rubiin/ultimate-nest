969cb5a5e42bbc6419f0b3c4120bceb7fc1f3069
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-01-21 01:17:00 +0545

    feat(unocss): added unocss

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import 'uno.css';
+import naive from 'naive-ui';
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(naive);
+
+app.use(createPinia());
+
+app.mount('#app');
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Unocss from 'unocss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		Unocss({
+			presets: [
+				presetUno(),
+				presetIcons({
+					extraProperties: {
+						display: 'inline-block',
+						'vertical-align': 'middle',
+					},
+				}),
+			],
+		}),
+	],
 });
