51dd9ad25a14059214eb1ecc5b668ec94baaca2d
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-04-08 22:07:11 +0545

    feat(adminjs): added user create

--- src/lib/adminjs/adminJs.config.ts
+++ src/lib/adminjs/adminJs.config.ts
@@ -2,6 +2,39 @@ import { AdminModuleFactory, CustomLoader } from "@adminjs/nestjs";
 import { User } from "@entities";
 import { MikroORM } from "@mikro-orm/core";
 
+const defaultPropertyList = {
+	createdAt: {
+		isVisible: {
+			show: false,
+			edit: false,
+		},
+	},
+	deletedAt: {
+		isVisible: {
+			show: false,
+			edit: false,
+		},
+	},
+	updatedAt: {
+		isVisible: {
+			show: false,
+			edit: false,
+		},
+	},
+	idx: {
+		isVisible: {
+			show: false,
+			edit: false,
+		},
+	},
+	id: {
+		isVisible: {
+			show: false,
+			edit: false,
+		},
+	},
+};
+
 export const adminjsConfig: AdminModuleFactory & CustomLoader = {
 	inject: [MikroORM],
 	useFactory: async (orm: MikroORM) => {
@@ -10,9 +43,10 @@ export const adminjsConfig: AdminModuleFactory & CustomLoader = {
 				rootPath: "/admin",
 				branding: {
 					companyName: "NestJS Admin",
+					logo: "https://rb.gy/p8apxe",
 				},
 				auth: {
-					authenticate: async (email, password) => ({
+					authenticate: async (email: any, password: any) => ({
 						email,
 						password,
 					}),
@@ -20,12 +54,35 @@ export const adminjsConfig: AdminModuleFactory & CustomLoader = {
 					cookiePassword: "testPass",
 				},
 				resources: [
-					{
-						model: User,
-						orm,
-					},
+					CreateCarResource(orm, User, {
+						roles: {
+							isVisible: {
+								show: false,
+								edit: false,
+							},
+						},
+					}),
 				],
 			},
 		};
 	},
+};
+
+export const CreateCarResource = (
+	orm: MikroORM,
+	model: any,
+	fieldOptions: any = {},
+) => {
+	return {
+		resource: {
+			model,
+			orm,
+		},
+		options: {
+			properties: {
+				...defaultPropertyList,
+				...fieldOptions,
+			},
+		},
+	};
 };
