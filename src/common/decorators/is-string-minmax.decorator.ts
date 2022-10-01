a4885c533e43e7548835a60c422e249f8561992b
bytegizmo <114669162+bytegizmo@users.noreply.github.com>
2022-10-01 10:58:11 +0545

    Update is-string-minmax.decorator.ts
    add missing isoptional

--- src/common/decorators/is-string-minmax.decorator.ts
+++ src/common/decorators/is-string-minmax.decorator.ts
@@ -2,6 +2,7 @@ import { applyDecorators } from "@nestjs/common";
 import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 import { i18nValidationMessage } from "nestjs-i18n";
 import { Sanitize } from "./sanitize.decorator";
+import { IsOptional } from "@common/validators/custom-optional.validator";
 
 export function IsStringMinMaxDecorator(required=true,min = 3, max = 255) {
 	const decoratorsToApply = [
