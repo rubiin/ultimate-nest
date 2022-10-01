d3278949154b2566fa53e9db9dbde4eca9b32bc1
bytegizmo <114669162+bytegizmo@users.noreply.github.com>
2022-10-01 10:53:57 +0545

    Update is-string-minmax.decorator.ts
    Add required option to mark strings as required and optional

--- src/common/decorators/is-string-minmax.decorator.ts
+++ src/common/decorators/is-string-minmax.decorator.ts
@@ -3,10 +3,9 @@ import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 import { i18nValidationMessage } from "nestjs-i18n";
 import { Sanitize } from "./sanitize.decorator";
 
-export function IsStringMinMaxDecorator(min = 3, max = 255) {
+export function IsStringMinMaxDecorator(required=true,min = 3, max = 255) {
 	const decoratorsToApply = [
 		Sanitize(),
-		IsNotEmpty({ message: i18nValidationMessage("validation.isNotEmpty") }),
 		IsString({
 			message: i18nValidationMessage("validation.isDataType", {
 				type: "string",
@@ -15,6 +14,8 @@ export function IsStringMinMaxDecorator(min = 3, max = 255) {
 		MinLength(min, { message: i18nValidationMessage("validation.minLength") }),
 		MaxLength(max, { message: i18nValidationMessage("validation.maxLength") }),
 	];
+
+	required ? decoratorsToApply.push(IsNotEmpty({ message: i18nValidationMessage("validation.isNotEmpty") })) : decoratorsToApply.push(IsOptional());
 
 	return applyDecorators(...decoratorsToApply);
 }
