63f5c8bd48b6d09d96484a21ba4944ec488dd312
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-11-23 17:17:43 +0545

    fix: usersubsrciber password hash

--- src/common/database/user.subscriber.ts
+++ src/common/database/user.subscriber.ts
@@ -1,20 +1,26 @@
 import { User } from "@entities";
-import { EntityName, EventArgs, EventSubscriber, Subscriber } from "@mikro-orm/core";
+import { EntityName, EventArgs, EventSubscriber } from "@mikro-orm/core";
+import { Injectable } from "@nestjs/common";
 import { hash } from "argon2";
 
-@Subscriber()
 /* It's a class that implements the EventSubscriber interface, and it's responsible for hashing the
 password before the user is created or updated */
+
+@Injectable()
 export class UserSubscriber implements EventSubscriber<User> {
 	getSubscribedEntities(): EntityName<User>[] {
 		return [User];
 	}
 
 	async beforeCreate(arguments_: EventArgs<User>): Promise<void> {
-		arguments_.entity.password = await hash(arguments_.entity.password);
+		if (arguments_.changeSet.payload?.password) {
+			arguments_.entity.password = await hash(arguments_.entity.password);
+		}
 	}
 
 	async beforeUpdate(arguments_: EventArgs<User>): Promise<void> {
-		arguments_.entity.password = await hash(arguments_.entity.password);
+		if (arguments_.changeSet.payload?.password) {
+			arguments_.entity.password = await hash(arguments_.entity.password);
+		}
 	}
 }
