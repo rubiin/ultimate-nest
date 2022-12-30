fc6c53b0d396f54457306593f8664aee90f05a8c
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-12-30 20:16:24 +0545

    fix:add redis connection pool

--- src/lib/cache/cache.module.ts
+++ src/lib/cache/cache.module.ts
@@ -1,20 +1,24 @@
 import { NestConfigModule } from "@lib/config/config.module";
-import { CacheModule, Global, Module } from "@nestjs/common";
+import { CacheModule, Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import redisStore from "cache-manager-redis-store";
-
+import type { ClientOpts } from 'redis';
 import { CacheService } from "./cache.service";
 
-@Global()
 @Module({
 	imports: [
-		CacheModule.registerAsync({
+		CacheModule.registerAsync<ClientOpts>({
 			imports: [NestConfigModule],
 			useFactory: async (configService: ConfigService) => ({
 				store: redisStore,
 				url: configService.get("redis.uri"),
 				ttl: configService.get<number>("redis.ttl", 10),
-				db: 0,
+				database: 0,
+				isGlobal: true,
+				isolationPoolOptions :{
+					max: 10,
+					min: 1,
+				}
 			}),
 			inject: [ConfigService],
 		}),
