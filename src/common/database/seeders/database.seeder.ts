19cec5e45ea4c062e3bcb40144fe44a1cadb45ce
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-01-29 20:24:49 +0545

    feat: add logger for seeder

--- src/common/database/seeders/database.seeder.ts
+++ src/common/database/seeders/database.seeder.ts
@@ -1,12 +1,20 @@
 import { EntityManager } from "@mikro-orm/core";
 import { Seeder } from "@mikro-orm/seeder";
+import { Logger } from "@nestjs/common";
 
 import { AdminSeeder } from "./admin.seeder";
 import { UserSeeder } from "./user.seeder";
 
+const logger = new Logger("DatabaseSeeder");
+
 /* It calls the AdminSeeder and UserSeeder classes */
 export class DatabaseSeeder extends Seeder {
 	async run(em: EntityManager): Promise<void> {
-		return this.call(em, [AdminSeeder, UserSeeder]);
+		const seeders = [AdminSeeder, UserSeeder];
+
+		logger.log("Seeding database with seeders: " + seeders.map(s => s.name).join(", ") + "");
+		logger.log("User password is set as : " + process.env.USER_PASSWORD);
+		
+return this.call(em, seeders);
 	}
 }
