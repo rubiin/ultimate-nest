d12a81eed474414f7e975fceb7f987581c52a934
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-02-04 22:23:04 +0545

    feat: add realip middlware for all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { CustomThrottlerGuard } from "@common/guards";
 import { CustomCacheInterceptor } from "@common/interceptors";
+import { RealIpMiddleware } from "@common/middlewares/ip.middleware";
 import { SharedModule } from "@modules/shared/shared.module";
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
 import { SentryInterceptor } from "@ntegral/nestjs-sentry";
 
@@ -22,4 +23,13 @@ import { SentryInterceptor } from "@ntegral/nestjs-sentry";
 		},
 	],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RealIpMiddleware)
+      .forRoutes({
+				path: "*",
+				method: RequestMethod.ALL,
+			});
+  }
+}
--- src/lib/pino/pino.module.ts
+++ src/lib/pino/pino.module.ts
@@ -8,6 +8,13 @@ import { LoggerModule } from "nestjs-pino";
 				return {
 					pinoHttp: {
 						name: "ultimate-nest",
+						serializers: {
+							req(request) {
+								request.body = request.raw.body;
+								
+return request;
+							},
+						},
 						level: process.env.NODE_ENV.startsWith("prod") ? "info" : "debug",
 						redact: {
 							paths: ["req.headers.authorization"],
