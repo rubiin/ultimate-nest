0e23498de0113c74e6220a8aea7955f42d5077d5
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-03-13 22:09:30 +0545

    fix: missing ports

--- src/lib/config/configs/mail.config.ts
+++ src/lib/config/configs/mail.config.ts
@@ -13,12 +13,25 @@ export const mail = registerAs("mail", () => ({
 }));
 
 export const mailConfigValidationSchema = {
-	MAIL_USERNAME: Joi.string().required(),
-	MAIL_PASSWORD: Joi.string().required(),
-	MAIL_HOST: Joi.string().required(),
-	MAIL_PORT: Joi.number().required(),
+	MAIL_SERVER: Joi.string().required().valid("SMTP", "SES"),
+	MAIL_USERNAME: Joi.string().when('MAIL_SERVER', { is: "SMTP", then: Joi.required() }),
+	MAIL_PASSWORD: Joi.string().when('MAIL_SERVER', { is: "SMTP", then: Joi.required() }),
+	MAIL_HOST: Joi.string().when('MAIL_SERVER', { is: "SMTP", then: Joi.required() }),
+	MAIL_PORT: Joi.number().when('MAIL_SERVER', { is: "SMTP", then: Joi.required() }),
 	MAIL_PREVIEW_EMAIL: Joi.boolean().default(false).required(),
 	MAIL_TEMPLATE_DIR: Joi.string().required(),
 	MAIL_SENDER_EMAIL: Joi.string().required(),
-	MAIL_SERVER: Joi.string().required(),
 };
+
+Joi.alternatives().conditional(Joi.object({ type: 1 }).unknown(), {
+	then: Joi.object({
+			type: Joi.number(),
+			firstname: Joi.string().required(),
+			lastname: Joi.string().required()
+	}),
+	otherwise: Joi.object({
+			type: Joi.number(),
+			salary: Joi.string().required(),
+			pension: Joi.string().required()
+	})
+})
