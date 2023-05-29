70fa50cd5cf0fa119e22b19fd575046086f2b3be
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-05-29 12:29:28 +0545

    Update eta.adapter.ts

--- src/lib/mailer/adapters/eta.adapter.ts
+++ src/lib/mailer/adapters/eta.adapter.ts
@@ -4,7 +4,11 @@ import type { EtaConfig } from "eta/dist/types/config";
 import { Adapter } from "./abstract.adapter";
 
 export class EtaAdapter implements Adapter {
-	constructor(private readonly options: Partial<EtaConfig>) {}
+	constructor(private readonly options: Partial<EtaConfig>) {
+  	configure({
+			views: `${__dirname}/../../${this.options.template.dir}/`,
+		});
+  }
 
 	compile(template: string, data: Record<string, any>): Promise<string> {
 		return renderFile(template, data, this.options) as Promise<string>;
