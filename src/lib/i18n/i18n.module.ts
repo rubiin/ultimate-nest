ef86e2eae985488851f0860d88dd088f93540b1a
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-09-19 17:19:32 +0545

    fix: async style for pino and i18n module

--- src/lib/i18n/i18n.module.ts
+++ src/lib/i18n/i18n.module.ts
@@ -2,31 +2,41 @@ import path from "node:path";
 
 import { Module } from "@nestjs/common";
 import { AcceptLanguageResolver, CookieResolver, HeaderResolver, I18nModule, QueryResolver } from "nestjs-i18n";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    I18nModule.forRoot({
-      fallbackLanguage: "en",
-      fallbacks: {
-        "np-*": "np",
-        "en-*": "en",
-        "np_*": "np",
-        "en_*": "en",
-        "en": "en",
-        "np": "np",
-      },
-      logging: true,
-      loaderOptions: {
-        path: path.join(__dirname, "../../resources/i18n/"),
-        watch: true,
-        includeSubfolders: true,
-      },
-      typesOutputPath: path.join(`${process.cwd()}/src/generated/i18n-generated.ts`),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService<Configs, true>) => ({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        fallbackLanguage: "en",
+        fallbacks: {
+          "np-*": "np",
+          "en-*": "en",
+          "np_*": "np",
+          "en_*": "en",
+          "en": "en",
+          "np": "np",
+        },
+        logging: true,
+        loaderOptions: {
+          path: path.join(__dirname, "../../resources/i18n/"),
+          watch: true,
+          includeSubfolders: true,
+        },
+        typesOutputPath: configService.get("app.env", { infer: true }).startsWith("prod") ? undefined : path.join(`${process.cwd()}/src/generated/i18n-generated.ts`),
+        resolvers: [
+          new HeaderResolver(["x-custom-lang"]),
+          AcceptLanguageResolver,
+          new CookieResolver(),
+          { use: QueryResolver, options: ["lang", "locale"] },
+        ],
+      }),
       resolvers: [
-        new HeaderResolver(["x-custom-lang"]),
+        { use: QueryResolver, options: ["lang"] },
         AcceptLanguageResolver,
-        new CookieResolver(),
-        { use: QueryResolver, options: ["lang", "locale"] },
+        new HeaderResolver(["x-lang"]),
       ],
     }),
   ],
--- src/lib/pino/pino.module.ts
+++ src/lib/pino/pino.module.ts
@@ -1,4 +1,5 @@
 import { Module, RequestMethod } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { LoggerModule } from "nestjs-pino";
 
 // Fields to redact from logs
@@ -12,84 +13,88 @@ const basePinoOptions = {
 
 @Module({
   imports: [
-    LoggerModule.forRoot({
+    LoggerModule.forRootAsync({
+      useFactory: (configService: ConfigService<Configs, true>) => ({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        pinoHttp: {
+          timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+          name: "ultimate-nest",
+          customProps: (_request, _response) => ({
+            context: "HTTP",
+          }),
+          serializers: {
+            req(request: Record<string, any>) {
+              request.body = request.raw.body;
 
-      pinoHttp: {
-        timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
-        name: "ultimate-nest",
-        customProps: (_request, _response) => ({
-          context: "HTTP",
-        }),
-        serializers: {
-          req(request: Record<string, any>) {
-            request.body = request.raw.body;
-
-            return request;
-          },
-        },
-        redact: {
-          paths: redactFields,
-          censor: "**GDPR COMPLIANT**",
-        },
-        transport: process.env.NODE_ENV.startsWith("prod")
-          ? {
-              targets: [
-                {
-                  target: "pino/file",
-                  level: "info", // log only errors to file
-                  options: {
-                    ...basePinoOptions,
-                    destination: "logs/info.log",
-                    mkdir: true,
-                    sync: false,
-                  },
-                },
-                {
-                  target: "pino/file",
-                  level: "error", // log only errors to file
-                  options: {
-                    ...basePinoOptions,
-                    destination: "logs/error.log",
-                    mkdir: true,
-                    sync: false,
-                  },
-                },
-              ],
-            }
-          : {
-              targets: [
-                {
-                  target: "pino-pretty",
-                  level: "info", // log only info and above to console
-                  options: {
-                    ...basePinoOptions,
-                    colorize: true,
-                  },
-                },
-                {
-                  target: "pino/file",
-                  level: "info", // log only errors to file
-                  options: {
-                    ...basePinoOptions,
-                    destination: "logs/info.log",
-                    mkdir: true,
-                    sync: false,
-                  },
-                },
-                {
-                  target: "pino/file",
-                  level: "error", // log only errors to file
-                  options: {
-                    ...basePinoOptions,
-                    destination: "logs/error.log",
-                    mkdir: true,
-                    sync: false,
-                  },
-                },
-              ],
+              return request;
             },
-      },
-      exclude: [{ method: RequestMethod.ALL, path: "doc" }],
+          },
+          redact: {
+            paths: redactFields,
+            censor: "**GDPR COMPLIANT**",
+          },
+          transport: configService.get("app.env", { infer: true }).startsWith("prod")
+            ? {
+                targets: [
+                  {
+                    target: "pino/file",
+                    level: "info", // log only errors to file
+                    options: {
+                      ...basePinoOptions,
+                      destination: "logs/info.log",
+                      mkdir: true,
+                      sync: false,
+                    },
+                  },
+                  {
+                    target: "pino/file",
+                    level: "error", // log only errors to file
+                    options: {
+                      ...basePinoOptions,
+                      destination: "logs/error.log",
+                      mkdir: true,
+                      sync: false,
+                    },
+                  },
+                ],
+              }
+            : {
+                targets: [
+                  {
+                    target: "pino-pretty",
+                    level: "info", // log only info and above to console
+                    options: {
+                      ...basePinoOptions,
+                      colorize: true,
+                    },
+                  },
+                  {
+                    target: "pino/file",
+                    level: "info", // log only errors to file
+                    options: {
+                      ...basePinoOptions,
+                      destination: "logs/info.log",
+                      mkdir: true,
+                      sync: false,
+                    },
+                  },
+                  {
+                    target: "pino/file",
+                    level: "error", // log only errors to file
+                    options: {
+                      ...basePinoOptions,
+                      destination: "logs/error.log",
+                      mkdir: true,
+                      sync: false,
+                    },
+                  },
+                ],
+              },
+        },
+        exclude: [{ method: RequestMethod.ALL, path: "doc" }],
+
+      }),
 
     }),
   ],
