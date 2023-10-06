88d56bf3450509273522ea44356181f90213e05e
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-10-06 15:25:18 +0545

    fix: concurrency options for pool

--- src/common/misc/pool.ts
+++ src/common/misc/pool.ts
@@ -7,6 +7,10 @@ const logger = new Logger("ThreadPool");
 
 /* Creating a new thread pool with half of available threads and a maximum of available threads. */
 export const pool = new DynamicThreadPool(Math.floor(availableParallelism() / 2), availableParallelism(), path.resolve(__dirname, "workers.js"), {
+  enableTasksQueue: true,
+  tasksQueueOptions: {
+    concurrency: 8
+  },
   errorHandler: error => logger.error(error),
   onlineHandler: () => logger.log("✅ Worker is online"),
-});
+})
