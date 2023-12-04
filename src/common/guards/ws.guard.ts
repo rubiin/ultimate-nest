757567694a8d8b38f2bd8aa2e6810bc7f82b2e20
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-12-04 17:31:31 +0545

    fix(ws.guard): translate exception messages

--- src/common/guards/ws.guard.ts
+++ src/common/guards/ws.guard.ts
@@ -7,6 +7,7 @@ import { User } from "@entities";
 import { BaseRepository } from "@common/database";
 import type { JwtPayload } from "@common/@types";
 import type { Socket } from "socket.io";
+import { translate } from "@lib/i18n";
 
 @Injectable()
 export class WsJwtGuard implements CanActivate {
@@ -20,14 +21,14 @@ export class WsJwtGuard implements CanActivate {
     const client = context.switchToWs().getClient<Socket>()?.handshake;
 
     if (!client.headers.authorization)
-      throw new WsException("Unauthorized");
+      throw new WsException(translate("exception.apiUnauthorizedResponse"));
     const token = client.headers.authorization;
 
     const payload: JwtPayload = await this.jwtService.verify(token);
     const user = await this.userRepository.findOne({ id: payload.sub });
 
     if (!user)
-      throw new WsException("Unauthorized");
+      throw new WsException(translate("exception.apiUnauthorizedResponse"));
 
     return true;
   }
