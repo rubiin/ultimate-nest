26e1a5e8f0c1681bedc4f8c669426ab4dfe1cfea
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-08-27 23:10:36 +0545

    fix: eslint strict null check

--- src/lib/pino/pino.module.ts
+++ src/lib/pino/pino.module.ts
@@ -13,89 +13,89 @@ const basePinoOptions = {
 @Module({
   imports: [
     LoggerModule.forRoot({
-        pinoHttp: {
-          timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
-          name: "ultimate-nest",
-          customProps: () => ({
-            context: "HTTP",
-          }),
-          serializers: {
-            req(request: {
+      pinoHttp: {
+        timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+        name: "ultimate-nest",
+        customProps: () => ({
+          context: "HTTP",
+        }),
+        serializers: {
+          req(request: {
+            body: Record<string, any>;
+            raw: {
               body: Record<string, any>;
-              raw: {
-                body: Record<string, any>;
-              };
-            }) {
-              request.body = request.raw.body;
+            };
+          }) {
+            request.body = request.raw.body;
 
-              return request;
-            },
+            return request;
           },
-          redact: {
-            paths: redactFields,
-            censor: "**GDPR COMPLIANT**",
-          },
-          transport: true
-            ? {
-                targets: [
-                  {
-                    target: "pino/file",
-                    level: "info", // log only errors to file
-                    options: {
-                      ...basePinoOptions,
-                      destination: "logs/info.log",
-                      mkdir: true,
-                      sync: false,
-                    },
-                  },
-                  {
-                    target: "pino/file",
-                    level: "error", // log only errors to file
-                    options: {
-                      ...basePinoOptions,
-                      destination: "logs/error.log",
-                      mkdir: true,
-                      sync: false,
-                    },
-                  },
-                ],
-              }
-            : {
-                targets: [
-                  {
-                    target: "pino-pretty",
-                    level: "info", // log only info and above to console
-                    options: {
-                      ...basePinoOptions,
-                      colorize: true,
-                    },
-                  },
-                  {
-                    target: "pino/file",
-                    level: "info", // log only errors to file
-                    options: {
-                      ...basePinoOptions,
-                      destination: "logs/info.log",
-                      mkdir: true,
-                      sync: false,
-                    },
-                  },
-                  {
-                    target: "pino/file",
-                    level: "error", // log only errors to file
-                    options: {
-                      ...basePinoOptions,
-                      destination: "logs/error.log",
-                      mkdir: true,
-                      sync: false,
-                    },
-                  },
-                ],
-              },
         },
-        exclude: [{ method: RequestMethod.ALL, path: "doc" }],
+        redact: {
+          paths: redactFields,
+          censor: "**GDPR COMPLIANT**",
+        },
+        transport: true
+          ? {
+              targets: [
+                {
+                  target: "pino/file",
+                  level: "info", // log only errors to file
+                  options: {
+                    ...basePinoOptions,
+                    destination: "logs/info.log",
+                    mkdir: true,
+                    sync: false,
+                  },
+                },
+                {
+                  target: "pino/file",
+                  level: "error", // log only errors to file
+                  options: {
+                    ...basePinoOptions,
+                    destination: "logs/error.log",
+                    mkdir: true,
+                    sync: false,
+                  },
+                },
+              ],
+            }
+          : {
+              targets: [
+                {
+                  target: "pino-pretty",
+                  level: "info", // log only info and above to console
+                  options: {
+                    ...basePinoOptions,
+                    colorize: true,
+                  },
+                },
+                {
+                  target: "pino/file",
+                  level: "info", // log only errors to file
+                  options: {
+                    ...basePinoOptions,
+                    destination: "logs/info.log",
+                    mkdir: true,
+                    sync: false,
+                  },
+                },
+                {
+                  target: "pino/file",
+                  level: "error", // log only errors to file
+                  options: {
+                    ...basePinoOptions,
+                    destination: "logs/error.log",
+                    mkdir: true,
+                    sync: false,
+                  },
+                },
+              ],
+            },
+      },
+      exclude: [{ method: RequestMethod.ALL, path: "doc" }],
 
-      }),
+    }),
   ],
   exports: [LoggerModule],
 })
