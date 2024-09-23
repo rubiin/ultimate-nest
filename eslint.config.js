626b6426e89781bef3aec39b150e6ebf00009b22
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-09-23 23:12:07 +0545

    fix: mikroorm injection issue

--- eslint.config.js
+++ eslint.config.js
@@ -9,5 +9,45 @@ module.exports = rubiin({
   jsonc: true, // enable jsonc rules
   markdown: false, // enable markdown rules
   gitignore: true, // enable gitignore rules,
-
+  typescript: {
+    tsconfigPath: "tsconfig.json"
+    },
+  overrides: {
+    test: {
+      "ts/unbound-method": "off",
+      "ts/no-unsafe-assignment": "off",
+      "ts/no-unsafe-call": "off",
+    },
+    typescript:
+    {
+      "ts/no-unsafe-assignment": "off",
+      "ts/no-unsafe-call": "off",
+      "ts/strict-boolean-expressions":"off",
+      "ts/no-misused-promises": [
+				"error",
+				{
+					checksVoidReturn: false,
+				},
+			],
+      "ts/no-floating-promises":["error",{
+        "ignoreIIFE": true
+      }],
+      "unicorn/prefer-top-level-await": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          ignore: [
+            "\\.e2e*",
+            "\\.spec*",
+            "\\.decorator*",
+            "\\*idx*",
+          ],
+          allowList: {
+            ProcessEnv: true,
+            UUIDParam: true,
+          },
+        },
+      ],
+    },
+  },
 });
